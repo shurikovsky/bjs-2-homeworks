a9c89b6389ab797253d4a8de85e7d2ec1410fd85
Kryukov Alexandr <shurikovsky@mail.ru>
2024-10-13 04:17:41 +0500

    home work 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant;
+  discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant === 0) {
+    arr[0] = - b / (2 * a);
+  } else if (discriminant > 0) {
+      arr[0] = ((-b + Math.sqrt(discriminant)) / (2 * a));
+      arr[1] = ((-b - Math.sqrt(discriminant))/ (2 * a));
+    }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentToShares, percentMonth, bodyCredit, paymentMonth, allSum;
+  percentToShares = percent / 100;
+  percentMonth = percentToShares / 12;
+  bodyCredit = amount - contribution;
+  paymentMonth = bodyCredit * (percentMonth + (percentMonth / ((Math.pow((1 + percentMonth), countMonths) - 1))));
+  allSum = parseFloat((paymentMonth * countMonths).toFixed(2));
+  return allSum;
 }
