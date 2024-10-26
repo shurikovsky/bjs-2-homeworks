fc31466bb3ba938112e4ab5f9d13b424777e9996
Kryukov Alexandr <shurikovsky@mail.ru>
2024-10-26 23:50:27 +0500

    exception: i don't know

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,46 @@
-﻿
+﻿function parseCount(count) {
+  let number = Number.parseFloat(count);   
+  if (isNaN(number)) {
+    throw new Error("Невалидное значение");
+  }
+  return number; 
+}
+
+function validateCount(count) {
+  try {
+    return parseCount(count);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+      if ((a + b) < c || (a + c) < b || (b + c) < a) {
+        throw new Error("Треугольник с такими сторонами не существует");
+      }
+      this.a = a;
+      this.b = b;
+      this.c = c;
+  }
+
+  get perimeter() {
+      return this.a + this.b + this.c;
+  }
+
+  get area() {
+      let partPerimeter = 0.5 * this.perimeter;
+      return +(Math.sqrt(partPerimeter * (partPerimeter - this.a) * (partPerimeter - this.b) * (partPerimeter - this.c))).toFixed(3);  
+  }
+}
+  
+function getTriangle(a, b, c) {
+  try {
+    let triangle = new Triangle(a, b, c);
+    return triangle;
+  } catch (error) {
+    return error
+  }
+}
+
+
